cc7f6175839e76a1b3eb2332deb0642a811f6757
Jed G <43355574+JedGascho@users.noreply.github.com>
2021-07-19 10:29:55 -0400

    Update and rename index.js to ear_trainer.js

--- ear_trainer.js
+++ ear_trainer.js
@@ -1,9 +1,13 @@
+// Used for synth input
+
 var noteValue = ["C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4"]
 
 var noteValueFreq = {
     "C4":"261.63", "C#4":"277.18", "D4":"293.66", "D#4":"311.13", "E4":"329.63", "F4":"349.23",
     "F#4":"369.99", "G4":"392.00", "G#4":"415.30", "A4":"440", "A#4":"466.16" 
 }
+
+// Global scope
 var osc = null;
 var osc2 = null;
 const merge = new Tone.Merge().toDestination();
@@ -11,7 +15,7 @@ var note1 = null;
 var note2 = null;
 
 
-
+// Plays and creates two notes
 playNote = function () {
     try {
         osc.stop();
@@ -28,43 +32,30 @@ playNote = function () {
     console.log(noteValue[note2]);
 
     //Playing the first note
-    osc = new Tone.Oscillator(noteValueFreq[noteValue[note1]], "sine").connect(merge, 0, 0).start();
+    const synth = new Tone.Synth().toDestination();
+    const synth2 = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease(noteValueFreq[noteValue[note1]], "4n");
     setTimeout(function(){
-        osc.stop()
-    },1000)
+        synth.triggerAttackRelease(noteValueFreq[noteValue[note1]], "4n");
+        synth2.triggerAttackRelease(noteValueFreq[noteValue[note2]], "4n");
+    },1250)
+    
 
-    setTimeout(function(){
-        osc = new Tone.Oscillator(noteValueFreq[noteValue[note1]], "sine").connect(merge, 0, 0).start();
-        osc2 = new Tone.Oscillator(noteValueFreq[noteValue[note2]], "sine").connect(merge, 0, 1).start();
-        setTimeout(function(){
-            osc.stop();
-            osc2.stop();
-        }, 1000);
-    },2000)
     document.getElementById('btn1').removeAttribute("disabled");
 }
 
+// Plays the sounds again
 function playAgain() {
-    try {
-        osc.stop();
-        osc2.stop();
-    } catch (error) {}   
-    //Playing the first note
-    osc = new Tone.Oscillator(noteValueFreq[noteValue[note1]], "sine").connect(merge, 0, 0).start();
+    const synth = new Tone.Synth().toDestination();
+    const synth2 = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease(noteValueFreq[noteValue[note1]], "4n");
     setTimeout(function(){
-        osc.stop()
-    },1000)
-
-    setTimeout(function(){
-        osc = new Tone.Oscillator(noteValueFreq[noteValue[note1]], "sine").connect(merge, 0, 0).start();
-        osc2 = new Tone.Oscillator(noteValueFreq[noteValue[note2]], "sine").connect(merge, 0, 1).start();
-        setTimeout(function(){
-            osc.stop();
-            osc2.stop();
-        }, 1000);
-    },2000)
+        synth.triggerAttackRelease(noteValueFreq[noteValue[note1]], "4n");
+        synth2.triggerAttackRelease(noteValueFreq[noteValue[note2]], "4n");
+    },1250)
 }
 
+// Random int gen, used for getting the randomized note
 function getRandomInt(min, max, note) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -79,7 +70,8 @@ function getRandomInt(min, max, note) {
     }
 }
 
-btnDisable = function() {
+// Disables buttons when sounds are playing
+btnDisable = function(wait) {
     document.getElementById('btn1').setAttribute('disabled', 'disabled');
     document.getElementById('btn2').setAttribute('disabled', 'disabled');
     document.getElementById('dropdown0').setAttribute('disabled', 'disabled');
@@ -93,6 +85,8 @@ btnDisable = function() {
     document.getElementById('dropdown8').setAttribute('disabled', 'disabled');
     document.getElementById('dropdown9').setAttribute('disabled', 'disabled');
     document.getElementById('dropdown10').setAttribute('disabled', 'disabled');
+    document.getElementById('equalTemp').setAttribute('disabled', 'disabled');
+    document.getElementById('meanTemp').setAttribute('disabled', 'disabled');
     setTimeout(function(){
         document.getElementById('btn1').removeAttribute('disabled');
         document.getElementById('btn2').removeAttribute('disabled');
@@ -107,13 +101,34 @@ btnDisable = function() {
         document.getElementById('dropdown8').removeAttribute('disabled');
         document.getElementById('dropdown9').removeAttribute('disabled');
         document.getElementById('dropdown10').removeAttribute('disabled');
-
-    }, 3200)
+        document.getElementById('equalTemp').removeAttribute('disabled');
+        document.getElementById('meanTemp').removeAttribute('disabled');
+    }, wait)
 }
 
+// Checks if users input is the correct answer
 userInput = function(note) {
     if (note === noteValue[note2]) {
-        alert('Nice Work!')
+        if (confirm('Nice Work!, play again?')) {
+            playNote();
+            btnDisable(3200);
+        } else {
+            document.getElementById('dropdown0').setAttribute('disabled', 'disabled');
+            document.getElementById('dropdown1').setAttribute('disabled', 'disabled');
+            document.getElementById('dropdown2').setAttribute('disabled', 'disabled');
+            document.getElementById('dropdown3').setAttribute('disabled', 'disabled');
+            document.getElementById('dropdown4').setAttribute('disabled', 'disabled');
+            document.getElementById('dropdown5').setAttribute('disabled', 'disabled');
+            document.getElementById('dropdown6').setAttribute('disabled', 'disabled');
+            document.getElementById('dropdown7').setAttribute('disabled', 'disabled');
+            document.getElementById('dropdown8').setAttribute('disabled', 'disabled');
+            document.getElementById('dropdown9').setAttribute('disabled', 'disabled');
+            document.getElementById('dropdown10').setAttribute('disabled', 'disabled');
+            document.getElementById('btn2').setAttribute('disabled', 'disabled');
+            document.getElementById('firstNote').innerHTML = "​​​ "
+
+        }
+        
     } else {
         alert('Not Quite')
     }
