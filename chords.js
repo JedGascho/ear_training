c70cc7e85c2926f046708e54a2044ffd957e39f4
Jed G <43355574+JedGascho@users.noreply.github.com>
2021-07-23 21:31:08 -0400

    Update chords.js
    cleaned up code, and adjusted for the new html file

--- chords.js
+++ chords.js
@@ -1,4 +1,52 @@
 // Plays the chord
+FS = ['equalTempF#M', 
+'meanTempF#M', 
+'werckmeisterTempF#M',
+'pythagoreanTempF#M',
+'justMajorTempF#M',
+'equalTempF#m',
+'meanTempF#m',
+'werckmeisterTempF#m',
+'pythagoreanTempF#m',
+'justMajorTempF#m'
+]
+
+G = ['equalTempGM', 
+'meanTempGM', 
+'werckmeisterTempGM',
+'pythagoreanTempGM',
+'justMajorTempGM',
+'equalTempGm',
+'meanTempGm',
+'werckmeisterTempGm',
+'pythagoreanTempGm',
+'justMajorTempGm'
+]
+
+GS = ['equalTempG#M', 
+'meanTempG#M', 
+'werckmeisterTempG#M',
+'pythagoreanTempG#M',
+'justMajorTempG#M',
+'equalTempG#m',
+'meanTempG#m',
+'werckmeisterTempG#m',
+'pythagoreanTempG#m',
+'justMajorTempG#m'
+]
+
+D = ['equalTempDM', 
+'meanTempDM', 
+'werckmeisterTempDM',
+'pythagoreanTempDM',
+'justMajorTempDM',
+'equalTempDm',
+'meanTempDm',
+'werckmeisterTempDm',
+'pythagoreanTempDm',
+'justMajorTempDm'
+]
+
 
 function playChord (one, two, three) {
     const synth = new Tone.Synth().toDestination();
@@ -21,4 +69,35 @@ function playChord (one, two, three) {
         synth2.triggerAttackRelease(two, "4n");
         synth3.triggerAttackRelease(three, "4n");
     }, 2300)
+}
+
+btnDisable = function(wait, x) {
+    for (x in FS) {
+        document.getElementById(FS[x]).setAttribute('disabled', 'disabled');
+    }
+    for (x in G) {
+        document.getElementById(G[x]).setAttribute('disabled', 'disabled');
+    }
+    for (x in GS) {
+        document.getElementById(GS[x]).setAttribute('disabled', 'disabled');
+    }
+    for (x in D) {
+        document.getElementById(D[x]).setAttribute('disabled', 'disabled');
+    }
+    setTimeout(function(){
+        for (x in FS) {
+            document.getElementById(FS[x]).removeAttribute('disabled');
+        }
+        for (x in G) {
+            document.getElementById(G[x]).removeAttribute('disabled');
+        }
+        for (x in GS) {
+            document.getElementById(GS[x]).removeAttribute('disabled');
+        }
+        for (x in D) {
+            document.getElementById(D[x]).removeAttribute('disabled');
+        }
+    }, wait)
+
+
 }
